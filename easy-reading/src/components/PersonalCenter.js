d05f13b4185cfe134d67bac4c06a0c93999bf587
MarhongBaLa <18821782939@163.com>
2019-04-30 17:21:15 +0800

    完成个人中心页面界面设计(设置头像没有实现)

--- easy-reading/src/components/PersonalCenter.js
+++ easy-reading/src/components/PersonalCenter.js
@@ -1,8 +1,32 @@
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-import {Tag} from 'antd';
-import { Menu ,Divider,Tabs,Table} from 'antd';
+import { Menu ,Divider,Tabs,Table,Select,Form,DatePicker,Input,Button,Tag} from 'antd';
+import moment from 'moment';
+import {WrappedRegistrationForm} from './RegistrationForm';
 require('../css/PersonalCenter.css');
+const { Option } = Select;
+const formItemLayout = {
+    labelCol: {
+        xs: { span: 24 },
+        sm: { span: 5 },
+    },
+    wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 12 },
+    },
+};
+const tailFormItemLayout = {
+    wrapperCol: {
+        xs: {
+            span: 24,
+            offset: 0,
+        },
+        sm: {
+            span: 16,
+            offset: 8,
+        },
+    },
+};
 // 菜单项为"我的书架"的表头
 const columns = [
     { title: '书名', dataIndex: 'name', key: 'name' },
@@ -114,6 +138,60 @@ const likeData = [
         key: 3, post: '苏铭好惨',  latReplyTime: '2019-02-15 10:15:10',bookName: '求魔',
     },
 ];
+// 菜单项为“消息通知” > “系统消息” 时的数据
+const sysData = [
+    {
+        key: 1, from: '管理员', msgTitle:"新年问候",time: '2019-03-15 14:20:20',content: '大家新年好',
+    },
+    {
+        key: 2, from: '管理员', msgTitle:"五一假期",time: '2019-03-12 10:20:20',content: '五一去打球',
+    },
+    {
+        key: 3, from: '管理员', msgTitle:"科目三考试",time: '2019-03-05 19:20:20',content: '科目二考完十五天才能考科三',
+    },
+];
+// 菜单项为“消息通知” > “私息” 时的数据
+const perData = [
+    {
+        key: 1, from: '小新', msgTitle:"新年问候",time: '2019-03-15 14:20:20',content: '大家新年好',
+    },
+    {
+        key: 2, from: '正南', msgTitle:"五一假期",time: '2019-03-12 10:20:20',content: '五一去打球',
+    },
+    {
+        key: 3, from: '风间', msgTitle:"科目三考试",time: '2019-03-05 19:20:20',content: '科目二考完十五天才能考科三',
+    },
+];
+// 菜单项为“阅读记录” > “周” 时的数据
+const weekData = [
+    {
+        key: 1, num: 3, totalTime: '144小时',period: '2019-03-04 至 2019-03-10',
+    },
+    {
+        key: 2, num: 3,  totalTime: '144小时',period: '2019-03-11 至 2019-03-17',
+    },
+    {
+        key: 3, num: 3,  totalTime: '144小时',period: '2019-03-18 至 2019-03-24',
+    },
+];
+// 菜单项为“阅读记录” > “月” 时的数据
+const monthData = [
+    {
+        key: 1, num: 9, totalTime: '432小时',period: '2019年3月',
+    },
+    {
+        key: 2, num: 9, totalTime: '432小时',period: '2019年4月',
+    },
+];
+// 菜单项为“阅读记录” > “年” 时的数据
+const yearData = [
+    {
+        key: 1, num: 18, totalTime: '864小时',period: '2019年',
+    },
+    {
+        key: 2, num: 30, totalTime: '1000小时',period: '2018年',
+    },
+];
 // 菜单项为“上传记录”时的数据
 const data6 = [
     {
@@ -147,10 +225,33 @@ const expandedRowRender = (e) => {
             ),
         },
     ];
-    console.log(e);
     const data = [{key: e.key+"剑来", name: '剑来'+e.key, author:"烽火戏诸侯",chapter: '第四百九十二章 别有洞天', updateTime: '2019-04-30 00:03:41'},
         {key: e.key+ '秦吏', name: '秦吏'+e.key,author:"七月新番", chapter: '第897章 我来看你了', updateTime: '2019-04-29 18:11:35'},
         {key: e.key+'汉乡', name: '汉乡'+e.key, author:"孑与2",chapter: '第一百六十八章 泰山对', updateTime: '2019-04-29 14:46:20'},];
+
+    return (
+        <Table
+            columns={columns}
+            dataSource={data}
+            pagination={false}
+        />
+    );
+};
+// 菜单项为“阅读记录”时每一行展开的内容
+const recordExpandedRowRender = (e) => {
+    const columns = [
+        { title: '书名', dataIndex: 'name', key: 'name' },
+        { title: '作者', dataIndex: 'author', key: 'author' },
+        { title: '类型', dataIndex: 'type',key: 'type'},
+        { title: '地区', dataIndex: 'distribute', key: 'distribute' },
+        { title: '年代', dataIndex: 'era', key: 'era' },
+        { title: '时长', dataIndex: 'totalTime',key: 'totalTime'},
+        { title: '开始阅读时间', dataIndex: 'startTime', key: 'startTime' },
+        { title: '最后阅读时间', dataIndex: 'lastTime', key: 'lastTime' },
+    ];
+    const data = [{key: e.key+"剑来", name: '剑来'+e.key, author:"烽火戏诸侯",type: '玄幻',distribute:"中国",era:"春秋战国",totalTime:"54小时", startTime: '2018-04-30 00:03:41', lastTime: '2019-04-30 00:03:41'},
+        {key: e.key+ '秦吏', name: '秦吏'+e.key,author:"七月新番",type: '历史',distribute:"中国",era:"秦汉",totalTime:"30小时", startTime: '2018-02-15 00:03:41', lastTime: '2019-04-30 00:03:41'},
+        {key: e.key+'汉乡', name: '汉乡'+e.key, author:"孑与2",type: '历史',distribute:"中国",era:"汉朝",totalTime:"60小时", startTime: '2018-09-30 00:03:41', lastTime: '2019-04-30 00:03:41'},];
 
     return (
         <Table
@@ -165,9 +266,11 @@ const postTabs = [{name:"发表的书评",dataName:"postData",columnsName:"postC
     {name:"点赞的书评",dataName:"likeData",columnsName:"likeColumns"}];
 const msgTabs = [{name:"系统消息",dataName:"sysData",columnsName:"sysColumns"},
     {name:"私信",dataName:"perData",columnsName:"perColumns"},];
-const readingRecord = [{name:"周",dataName:"weekData",columnsName:"weekColumns"},
+const readingRecord = [{name:"周",dataName:"weekData",columnsName:"weekColumns",key:"weekdata"},
     {name:"月",dataName:"monthData",columnsName:"monthColumns"},
     {name:"年",dataName:"yearData",columnsName:"yearColumns"},];
+const perInfoTabs =  [{name:"基本信息",},
+    {name:"修改密码",},];
 const columnsCollection ={
     postColumns:[
         { title: '书评', dataIndex: 'post', key: 'post' },
@@ -184,56 +287,136 @@ const columnsCollection ={
         { title: '书评', dataIndex: 'post', key: 'post' },
         { title: '点赞时间', dataIndex: 'latReplyTime', key: 'latReplyTime' },
         { title: '所在书评区', dataIndex: 'bookName', key: 'bookName' },],
+    sysColumns:[ { title: '来自', dataIndex: 'from', key: 'from' },
+        { title: '标题', dataIndex: 'msgTitle', key: 'msgTitle' },
+        { title: '内容', dataIndex: 'content', key: 'content' },
+        { title: '日期', dataIndex: 'time', key: 'time' },],
+    perColumns:[ { title: '来自', dataIndex: 'from', key: 'from' },
+        { title: '标题', dataIndex: 'msgTitle', key: 'msgTitle' },
+        { title: '内容', dataIndex: 'content', key: 'content' },
+        { title: '日期', dataIndex: 'time', key: 'time' },],
+    weekColumns:[ { title: '阅读书籍数量', dataIndex: 'num', key: 'num' },
+        { title: '阅读总时长', dataIndex: 'totalTime', key: 'totalTime' },
+        { title: '起止日期', dataIndex: 'period', key: 'period' },],
+    monthColumns:[ { title: '阅读书籍数量', dataIndex: 'num', key: 'num' },
+        { title: '阅读总时长', dataIndex: 'totalTime', key: 'totalTime' },
+        { title: '起止日期', dataIndex: 'period', key: 'period' },],
+    yearColumns:[ { title: '阅读书籍数量', dataIndex: 'num', key: 'num' },
+        { title: '阅读总时长', dataIndex: 'totalTime', key: 'totalTime' },
+        { title: '起止日期', dataIndex: 'period', key: 'period' },],
 }
 export default class PersonalCenter extends Component{
+    static defaultProps = {
+        userInfo :{name:"麦香馅饼和肉夹馍",gender:"male",address:"shanxi,xian,lianhu",birthday:"2019-04-28",phone:"15245256",email:"15245256@qq.com",description:"我爱看小说你呢？"}
+    }
     constructor(props){
         super(props);
         this.state = {
             columns : columns,
             data:data,
             expandedRowRender:null,
+            recordExpandedRowRender:null,
             tabs:postTabs,
             postData:postData,
             replyData:replyData,
             likeData:likeData,
+            sysData:sysData,
+            perData:perData,
+            weekData:weekData,
+            monthData:monthData,
+            yearData:yearData,
+
+
         }
     }
     handleClick = (e) =>{
+
+        setTimeout(()=>{
+            this.setState({...this.state,recordExpandedRowRender:null});
+        },1);
         let table = ReactDOM.findDOMNode(this.table);
+        let perinfo = ReactDOM.findDOMNode(this.perinfo);
+        perinfo.classList.remove("ant-menu-item-selected");
         switch (e.key){
             case "1":
-                this.tabs.style.visibility = "none";
+                this.tabs.style.display = "none";
 
                 table.style.display = "block";
-                this.setState({columns:columns,data:data,expandedRowRender:null});
+
+                this.setState({...this.state,columns:columns,data:data,expandedRowRender:null});
                 break;
 
             case "2":
                 this.tabs.style.display = "none";
-
                 table.style.display = "block";
-                this.setState({columns:columns2,data:data2,expandedRowRender:expandedRowRender});
+
+                this.setState({...this.state,columns:columns2,data:data2,expandedRowRender:expandedRowRender});
                 break;
             case "3":
                 this.tabs.style.display = "block";
                 table.style.display = "none";
+
+                this.setState({...this.state,tabs:postTabs});
                 break;
+            case "4":
+                this.tabs.style.display = "block";
+                table.style.display = "none";
+
+                this.setState({...this.state,tabs:msgTabs});
+                break;
+            case "5":
+                 this.tabs.style.display = "block";
+                 table.style.display = "none";
+
+                setTimeout(()=>{
+                    this.setState({...this.state,tabs:readingRecord,recordExpandedRowRender:recordExpandedRowRender});
+                },2);
+                 break;
+
             case "6":
                 this.tabs.style.display = "none";
                 table.style.display = "block";
+
                 this.setState({columns:columns6,data:data6,expandedRowRender:null});
                 break;
             default:
-
+                this.tabs.style.display = "block";
+                table.style.display = "none";
+                this.setState({...this.state,tabs:perInfoTabs});
+                break;
         }
     }
+    handleTabChange = (key) => {
+        console.log(key);
+        if(key === "weekdata"){
+            this.setState({...this.state,recordExpandedRowRender:recordExpandedRowRender})
+        }else{
+            this.setState({...this.state,recordExpandedRowRender:null})
+        }
+}
+    handleEditInfo(e){
+        setTimeout(()=>{
+            this.setState({...this.state,recordExpandedRowRender:null});
+        },1);
+        let table = ReactDOM.findDOMNode(this.table);
+        this.tabs.style.display = "block";
+        table.style.display = "none";
+        this.setState({...this.state,tabs:perInfoTabs});
+        let perinfo = ReactDOM.findDOMNode(this.perinfo);
+        let items = perinfo.parentNode.children;
+        for(let item of items){
+            item.classList.remove("ant-menu-item-selected");
+        }
+        perinfo.classList.add("ant-menu-item-selected");
+    }
     render(){
+        const user = this.props.userInfo;
         return(
             <div className="wrap">
                 <div className="userInfo">
                     <img className="userImg" src="http://static.zongheng.com/userimage/default/image_120_120.gif"/>
                     <div className="userInfoMsg">
-                        <p><span className="userName"><strong>麦香馅饼和肉夹馍</strong></span><span className="modifyInfo"><i className="iconfont icon-xiugai"/> 个人资料修改</span></p>
+                        <p><span className="userName"><strong>麦香馅饼和肉夹馍</strong></span><span className="modifyInfo" onClick={this.handleEditInfo.bind(this)}><i className="iconfont icon-xiugai"/> 个人资料修改</span></p>
                         <p className="description">我是个阿斯蒂芬阿斯蒂芬啊啊士大夫阿斯蒂芬</p>
 
                         <span className="userTags"><Tag color="magenta">magenta</Tag>
@@ -257,7 +440,7 @@ export default class PersonalCenter extends Component{
                             <Menu.Item key="4"><i className="iconfont icon-xiaoxi"/> 消息通知</Menu.Item>
                             <Menu.Item key="5"><i className="iconfont icon-yuedujilu"/> 阅读记录</Menu.Item>
                             <Menu.Item key="6"><i className="iconfont icon-shujushangchuanjilu"/> 上传记录</Menu.Item>
-                            <Menu.Item key="7"><i className="iconfont icon-gerenziliao"/> 个人资料</Menu.Item>
+                            <Menu.Item key="7" ref={(perinfo) => this.perinfo = perinfo}><i className="iconfont icon-gerenziliao"/> 个人资料</Menu.Item>
                         </Menu>
                     </div>
                     <div className="main">
@@ -268,13 +451,20 @@ export default class PersonalCenter extends Component{
                             ref={(table) => this.table = table}
                         />
                         <div className="card-container" ref={(tabs) => this.tabs =tabs } style={{display:"none"}}>
-                            <Tabs type="card">
+                            <Tabs type="card" onChange={this.handleTabChange}>
                                 {this.state.tabs.map((tab,index) => {
-                                    return <TabPane tab={tab.name} key={index+1}>
-                                        <Table columns={columnsCollection[tab.columnsName]} dataSource={this.state[tab.dataName]} />
+                                    return <TabPane tab={tab.name} key={tab.key || index+1}>
+                                        {(tab.name === "基本信息") || (tab.name === "修改密码") ?
+                                                <WrappedRegistrationForm user={user} type={tab.name}/>
+                                            :
+                                                <Table columns={columnsCollection[tab.columnsName]}
+                                                       dataSource={this.state[tab.dataName]}
+                                                       expandedRowRender={this.state.recordExpandedRowRender}
+                                            />}
                                     </TabPane>
                                 })}
                             </Tabs>
+
                         </div>
                     </div>
                 </div>
---
+++ easy-reading/src/components/RegistrationForm.js
@@ -0,0 +1,229 @@
+import { Form, Input, Tooltip, Icon, Cascader, Select,  Button, } from  'antd';
+import React,{Component} from 'react';
+const { Option } = Select;
+
+const residences = [{
+    value: 'zhejiang',
+    label: '浙江',
+    children: [{
+        value: 'hangzhou',
+        label: '杭州',
+        children: [{
+            value: 'xihu',
+            label: '西湖区',
+        }],
+    }],
+}, {
+    value: 'shanxi',
+    label: '陕西',
+    children: [{
+        value: 'xian',
+        label: '西安',
+        children: [{
+            value: 'lianhu',
+            label: '莲湖区',
+        }],
+    }],
+}];
+
+class RegistrationForm extends Component {
+
+    state = {
+        confirmDirty: false,
+    };
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    }
+    handleConfirmBlur = (e) => {
+        const value = e.target.value;
+        this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+    }
+
+    compareToFirstPassword = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && value !== form.getFieldValue('password')) {
+            callback('两次输入密码不一致!');
+        } else {
+            callback();
+        }
+    }
+
+    validateToNextPassword = (rule, value, callback) => {
+        const form = this.props.form;
+        if (value && this.state.confirmDirty) {
+            form.validateFields(['confirm'], { force: true });
+        }
+        callback();
+    }
+    render() {
+        const user = this.props.user;
+        const type = this.props.type;
+        const { getFieldDecorator } = this.props.form;
+
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 15 },
+                sm: { span: 8 },
+            },
+            wrapperCol: {
+                xs: { span: 15 },
+                sm: { span: 10 },
+            },
+        };
+        const tailFormItemLayout = {
+            wrapperCol: {
+                xs: {
+                    span: 15,
+                    offset: 0,
+                },
+                sm: {
+                    span: 10,
+                    offset: 8,
+                },
+            },
+        };
+        const prefixSelector = getFieldDecorator('prefix', {
+            initialValue: '86',
+        })(
+            <Select style={{ width: 70 }}>
+                <Option value="86">+86</Option>
+                <Option value="87">+87</Option>
+            </Select>
+        );
+        if(type === "基本信息"){
+            return (
+                <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+
+                    <Form.Item
+                        label={(
+                            <span>
+              昵称&nbsp;
+                                <Tooltip title="想让别人怎么称呼你?">
+                <Icon type="question-circle-o" />
+              </Tooltip>
+            </span>
+                        )}
+                    >
+                        {getFieldDecorator('nickname', {
+                            rules: [{ required: true, message: '昵称不能为空!', whitespace: true }],
+                        })(
+                            <Input placeholder={user.name}/>
+                        )}
+                    </Form.Item>
+                    <Form.Item
+                        label="性别">
+                        <Select defaultValue={user.gender}>
+                            <Option value="male">男</Option>
+                            <Option value="female">女</Option>
+                        </Select>
+                    </Form.Item>
+                    <Form.Item
+                        label="现居地"
+                    >
+                        {getFieldDecorator('residence', {
+                            initialValue: [user.address.split(",")[0],user.address.split(",")[1],user.address.split(",")[2]],
+                            rules: [{ type: 'array',  message: '请选择现居地!' }],
+                        })(
+                            <Cascader options={residences} />
+                        )}
+                    </Form.Item>
+                    <Form.Item
+                        label="E-mail"
+                    >
+                        {getFieldDecorator('email', {
+                            rules: [{
+                                type: 'email', message: '邮箱格式错误!',
+                            }, {
+                                message: '请输入邮箱!',
+                            }],
+                        })(
+                            <Input placeholder={user.email}/>
+                        )}
+                    </Form.Item>
+                    <Form.Item
+                        label="手机号码"
+                    >
+                        {getFieldDecorator('phone', {
+                            rules: [{  message: '请输入手机号码!' ,type:'number'}],
+                        })(
+                            <Input addonBefore={prefixSelector} placeholder={user.phone}/>
+                        )}
+                    </Form.Item>
+                    <Form.Item
+                        label="个人简介"
+
+                    >
+                        {getFieldDecorator('description', {
+                            rules: [{  message: '请输入个人简介!', whitespace: true }],
+                        })(
+                            <Input placeholder={user.description}/>
+                        )}
+                    </Form.Item>
+
+                    <Form.Item {...tailFormItemLayout}>
+                        <Button type="primary" htmlType="submit">保存</Button>
+                    </Form.Item>
+                </Form>
+            );
+        }else{
+            return (
+                <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+                    <Form.Item
+                        label="昵称"
+                    >
+                        <p>{user.name}</p>
+                    </Form.Item>
+                    <Form.Item
+                        label="旧密码"
+                    >
+                        {getFieldDecorator('oldPassword', {
+                            rules: [{
+                                required: true, message: '请输入旧密码!',
+                            },],
+                        })(
+                            <Input type="password" />
+                        )}
+                    </Form.Item>
+                <Form.Item
+                    label="新密码"
+                >
+                    {getFieldDecorator('password', {
+                        rules: [{
+                            required: true, message: '请输入新密码!',
+                        }, {
+                            validator: this.validateToNextPassword,
+                        }],
+                    })(
+                        <Input type="password" />
+                    )}
+                </Form.Item>
+                <Form.Item
+            label="确认密码"
+                >
+                {getFieldDecorator('confirm', {
+                rules: [{
+                    required: true, message: '请确认密码!',
+                }, {
+                    validator: this.compareToFirstPassword,
+                }],
+            })(
+                <Input type="password" onBlur={this.handleConfirmBlur} />
+            )}
+        </Form.Item>
+                    <Form.Item {...tailFormItemLayout}>
+                        <Button type="danger" htmlType="submit">修改</Button>
+                    </Form.Item>
+                </Form>
+            );
+        }
+
+    }
+}
+
+export const WrappedRegistrationForm = Form.create({ name: 'register' })(RegistrationForm);
+
